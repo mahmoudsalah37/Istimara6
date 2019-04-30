c095095e92c2406104652ec878eb0a2d6ccf5f1c
mahmoudsalah37 <mahmoudsalah37@gmail.com>
2019-04-30 11:23:45 -0700

    start

---
+++ MenuAnimation/Controls/Fixed Data/Child/UCAssistant.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Astmara6Con.Controls
+{
+    /// <summary>
+    /// Interaction logic for asisinfo.xaml
+    /// </summary>
+    public partial class UCAssistant : UserControl
+    {
+        string STRNamePage;
+        readonly FRMMainWindow Form = Application.Current.Windows[0] as FRMMainWindow;
+        public UCAssistant()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Form.gridShow.Children.Clear();
+            Form.gridShow.Children.Add(new UCDoctors());
+            STRNamePage = " الدكاترة";
+            Form.ChFormName(STRNamePage);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Form.gridShow.Children.Clear();
+            Form.gridShow.Children.Add(new UCCourses());
+            STRNamePage = "المقرارت";
+            Form.ChFormName(STRNamePage);
+        }
+    }
+}
